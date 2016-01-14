43dac373b7e9da526ba0774e11c0e2aef4f10291
CDGJosh <cdg.josh@googlemail.com>
2016-01-14 21:28:31 +0100

    initial: edit user

--- ajax/edituser.php
+++ ajax/edituser.php
@@ -1,16 +1,20 @@
 <?php 
 
-//	param		| method	| type		| desc
+//	param			| method	| type		| desc
 //------------------------------------------------------------------
-//	prename		| post		| string	| pername of new user
+//	prename			| post		| string	| pername of new user
 //------------------------------------------------------------------
-//	lastname	| post		| string	| lastname of new user
+//	lastname		| post		| string	| lastname of new user
 //------------------------------------------------------------------
-//	type		| post		| int		| type of new user
+//	type			| post		| int		| type of new user
 //------------------------------------------------------------------
-//	group		| post		| int		| group of new user
+//	group			| post		| int		| group of new user
 //------------------------------------------------------------------
-//	id			| post		| int		| id of user to edit
+//	com				| post		| int		| id of comitee
+//------------------------------------------------------------------
+//	id				| post		| int		| id of user to edit
+//------------------------------------------------------------------
+//	initialTeacher	| post		| bool		| true if user was initial Teacher
 //------------------------------------------------------------------
 $include = false;
 if(isset($rootfolder))
@@ -23,32 +27,62 @@ if(!$include)
 
 	include($_SERVER['DOCUMENT_ROOT'].$rootfolder."loginprotection.php");
 	
-	if(isset($_POST['id']))
+	//todo: permission checkdate
+	//todo: check if user exists
+	
+	if(intval($_POST['type']) == 1)
 	{
-		$username = substr($_POST['lastname'], 0, 3).substr($_POST['prename'], 0, 3);
-		$sql = "SELECT * FROM `user` WHERE `username` = '".mysql_real_escape_string($username)."';";
-		$res = mysql_query($sql) or die ("{\"status\":500, \"message\":\"ERROR #010 Query failed: $sql @ajax/adduser.php - ".mysql_error()."\"}");
-		
-		if(mysql_num_rows($res) != 0)
+		if(intval($_POST['initialTeacher']) == 1)
 		{
-			$username .= mysql_num_rows($res);
+			//teacher stays teacher, so just update
+			$sql = "UPDATE `teacher` SET ".
+			"`name`='".mysql_real_escape_string($_POST['lastname'])."',".
+			"`prename`='".mysql_real_escape_string($_POST['prename'])."' ".
+			" WHERE `id`='".mysql_real_escape_string($_POST['id'])."'";
+			mysql_query($sql) or die ("{\"status\":500, \"message\":\"ERROR #012 Query failed: $sql @ajax/adduser.php - ".mysql_error()."\"}");
 		}
-		
-		$sql = "INSERT INTO `user`(`name`, `prename`, `username`, `password`, `admin`, `style`, `stillthere`, `group`) VALUES ('".mysql_real_escape_string($_POST['lastname']).
-		"','".mysql_real_escape_string($_POST['prename'])."','".mysql_real_escape_string($username)."','".mysql_real_escape_string(md5("BAUM"))."','0','0','1','".mysql_real_escape_string($_POST['group'])."')";
-		mysql_query($sql) or die ("{\"status\":500, \"message\":\"ERROR #011 Query failed: $sql @ajax/adduser.php - ".mysql_error()."\"}");
+		else
+		{
+			//user turned teacher, insert teacher, delete user
+			$sql = "INSERT INTO `teacher`(`name`, `prename`, `sub0`, `sub1`, `sub2`, `sub3`, `visible`) VALUES ('".mysql_real_escape_string($_POST['lastname']).
+			"','".mysql_real_escape_string($_POST['prename'])."','','','','','1')";
+			mysql_query($sql) or die ("{\"status\":500, \"message\":\"ERROR #012 Query failed: $sql @ajax/adduser.php - ".mysql_error()."\"}");
+			
+			$sql = "DELETE FROM `user` WHERE `id`='".mysql_real_escape_string($_POST['id'])."';";
+			mysql_query($sql) or die ("{\"status\":500, \"message\":\"ERROR #011 Query failed: $sql @ajax/adduser.php - ".mysql_error()."\"}");
+		}
 	}
 	else
 	{
-		$sql = "INSERT INTO `teacher`(`name`, `prename`, `sub0`, `sub1`, `sub2`, `sub3`, `visible`) VALUES ('".mysql_real_escape_string($_POST['lastname']).
-		"','".mysql_real_escape_string($_POST['prename'])."','','','','','1')";
-		mysql_query($sql) or die ("{\"status\":500, \"message\":\"ERROR #012 Query failed: $sql @ajax/adduser.php - ".mysql_error()."\"}");
+		if(intval($_POST['initialTeacher']) == 1)
+		{
+			//teacher turned user, insert user, delete teacher
+			$sql = "INSERT INTO `user`(`name`, `prename`, `username`, `password`, `admin`, `style`, `stillthere`, `group`) VALUES ('".mysql_real_escape_string($_POST['lastname']).
+					"','".mysql_real_escape_string($_POST['prename'])."','".mysql_real_escape_string($username)."','".mysql_real_escape_string(md5(strval(mt_rand())."BAUM"))."','0','0','1','".mysql_real_escape_string($_POST['group'])."')";
+			mysql_query($sql) or die ("{\"status\":500, \"message\":\"ERROR #011 Query failed: $sql @ajax/adduser.php - ".mysql_error()."\"}");
+			
+			$sql = "DELETE FROM `teacher` WHERE `id`='".mysql_real_escape_string($_POST['id'])."';";
+			mysql_query($sql) or die ("{\"status\":500, \"message\":\"ERROR #011 Query failed: $sql @ajax/adduser.php - ".mysql_error()."\"}");
+		}
+		else
+		{
+			//user stays user, so just update
+			$sql = "UPDATE `user` SET `name`='".mysql_real_escape_string($_POST['lastname'])."',".
+					"`prename`='".mysql_real_escape_string($_POST['prename'])."',".
+					"`group`='".mysql_real_escape_string($_POST['group'])."',".
+					"`com`='".mysql_real_escape_string($_POST['com'])."'".
+					" WHERE `id`='".mysql_real_escape_string($_POST['id'])."'";
+					
+			mysql_query($sql) or die ("{\"status\":500, \"message\":\"ERROR #011 Query failed: $sql @ajax/adduser.php - ".mysql_error()."\"}");
+		}
+
+		
 	}
-	die("{\"status\":200, \"message\":\"Nutzer hinzugefügt!\"}");
+	die("{\"status\":200, \"message\":\"Nutzer bearbeitet!\"}");
 }
 else
 {
-	die("{\"status\":406, \"message\":\"no type specified\"}");
+	die("{\"status\":406, \"message\":\"blah\"}");
 }
 	
 
